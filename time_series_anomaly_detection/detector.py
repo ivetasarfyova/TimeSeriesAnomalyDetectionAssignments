8256b27bca64b7a33ddc644d91f835c3d088ec29
Bc. Iveta Šárfyová <sarfyive@fit.cvut.cz>
2021-04-30 20:56:37 +0200

    GAN_AD class

--- time_series_anomaly_detection/detector.py
+++ time_series_anomaly_detection/detector.py
@@ -1,6 +1,16 @@
 from typing import Optional, Iterable
-
 import pandas as pd
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.losses import BinaryCrossentropy
+from tensorflow.keras.layers import *
+from tensorflow.keras.optimizers import *
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
+from datetime import date
+import random
 
 from time_series_anomaly_detection.abstractions import (
     TimeSeriesAnomalyDetector
@@ -26,17 +36,245 @@ class GAN_AD(TimeSeriesAnomalyDetector):
 
     def __init__(
         self,
-        id_columns: Optional[Iterable[str]] = None,
+        window_size: int,
+        shift: int,
+        batch_size: int,
+        latent_dim: int,
+        n_features: int,
+        id_columns: Optional[Iterable[str]] = None
     ):
         super().__init__()
+        self._window_size = window_size
+        self._shift = shift
+        self._batch_size = batch_size
+        self._latent_dim = latent_dim
+        self._n_features = n_features
         self._id_columns = id_columns
+        
+        self._scaler = StandardScaler()
+        self._bce = BinaryCrossentropy()
+        
+        self._gan_ad = self._build_gan_ad()
+    
+    def _build_discriminator(self):
+        data = Input(shape=(self._window_size, self._n_features))
 
+        x = LSTM(150)(data)
+        x = Dense(50)(x)
+        x = BatchNormalization()(x)
+        x = LeakyReLU()(x)
+        x = Dropout(0.2)(x)
+
+        decision = Dense(1, activation = 'sigmoid')(x)
+
+        model = keras.Model(inputs=[data], outputs=[decision], name='discriminator')
+        return model
+    
+    def _build_generator(self):
+        z = Input(shape=(self._window_size, self._latent_dim))
+
+        x = Conv1D(30, padding='same', kernel_size=3) (z)
+        x = BatchNormalization()(x)
+        x = LeakyReLU()(x)
+        x = LSTM(200)(z)
+        x = Dense(self._window_size*self._n_features, activation = 'linear')(x)
+
+        series = Reshape((self._window_size, self._n_features))(x)
+
+        model = keras.Model(inputs=[z], outputs=[series], name='generator')
+        return model
+    
+    def _build_gan_ad(self):
+        generator = self._build_generator()
+        discriminator = self._build_discriminator()
+
+        z = Input(shape=(self._window_size, self._latent_dim))
+
+        generated_series = generator([z])
+        generated_series_eval = discriminator([generated_series])
+
+        model = keras.Model(inputs=[z], outputs=[generated_series, generated_series_eval], name='gan')
+        return model
+    
+    def _get_discriminator_loss(self, real, generated):
+        real_loss = self._bce(tf.ones_like(real), real)
+        generated_loss = self._bce(tf.zeros_like(generated), generated)
+        d_loss = real_loss + generated_loss
+        return d_loss
+    
+    def _get_generator_loss(self, generated):
+        return self._bce(tf.ones_like(generated), generated)
+    
+    @tf.function
+    def _fit_batch(self, batch, z):
+        with tf.GradientTape(persistent=True) as gt:
+
+            generated_series = self._generator(z, training = True)
+
+            generated_series_eval = self._discriminator(generated_series, training = True)
+            real_series_eval = self._discriminator(batch, training = True)
+
+            g_loss = self._get_generator_loss(generated_series_eval)
+            d_loss = self._get_discriminator_loss(real_series_eval, generated_series_eval)
+
+        discriminator_gradients = gt.gradient(d_loss, self._discriminator.trainable_variables)
+        generator_gradients = gt.gradient(g_loss, self._generator.trainable_variables)
+
+        self._discriminator_optimizer.apply_gradients(zip(discriminator_gradients, self._discriminator.trainable_variables))
+        self._generator_optimizer.apply_gradients(zip(generator_gradients, self._generator.trainable_variables))
+
+        return (d_loss, g_loss)
+    
+    def save_weights(self, file_name):
+        self._gan_ad.save_weights(file_name)
+    
+    def _train(self, dataset, epochs, enable_prints=False):
+        loss_history = []
+        for epoch in range(epochs):
+            batch_loss_history = []
+            for batch in dataset:
+                z = tf.random.normal([self._batch_size, self._window_size, self._latent_dim])
+                d_loss, g_loss = self._fit_batch(batch, z)
+                batch_loss_history.append([d_loss.numpy(), g_loss.numpy()])
+            loss_history.append(np.mean(batch_loss_history, axis = 0))
+            if ((epoch != 0) & (epoch%500 == 0) & enable_prints):
+                print(epoch, ". epoch")
+                print("Batch loss (disc_loss, gen_loss) :", np.mean(batch_loss_history, axis=0))
+                plt.plot(np.array([loss[0] for loss in loss_history]), color='green')
+                plt.plot(np.array([loss[1] for loss in loss_history]), color='blue')
+                plt.show()
+
+                print('Real window')
+                window = pd.DataFrame(dataset.__iter__().__next__()[0].numpy())
+                window.plot(figsize=(10,5))
+                plt.show()
+
+                z = tf.random.normal([self._batch_size, self._window_size, self._latent_dim])
+                generated_series = self._generator(z, training = True)
+                print('Generated window')
+                window = pd.DataFrame(generated_series[0].numpy())
+                window.plot(figsize=(10,5))
+                plt.show()
+
+                print('Generated windows - possible mode collapse visualisation')
+                fig, axs = plt.subplots(3, 3,figsize=(15,15))
+                for i in range(9):
+                    z_init = tf.random.normal([self._window_size, self._latent_dim])
+                    generated_sample = self._generator(tf.reshape(z_init, [1, self._window_size, self._latent_dim]), training = False)[0]
+                    axs[int(i%3), int(i/3)].plot(generated_sample.numpy())
+                plt.show()
+                print()
+            if (epoch%1000 == 0):
+                self.save_weights("gan_ad_" + str(date.today()) + "_epoch_" + str(epoch) + ".h5")
+
+    def _preprocess_data(self, data: pd.DataFrame):
+        # TODO remove idcols
+        data_scaled = self._scaler.fit_transform(data)
+        # TODO for different time series types
+        # TODO remove seed
+        dataset = tf.keras.preprocessing.timeseries_dataset_from_array(data_scaled.astype(np.float),
+                                                               targets=None, sequence_length=self._window_size,
+                                                               sequence_stride=self._shift, sampling_rate=1,
+                                                               batch_size=self._batch_size, shuffle=True, seed=6)
+        return dataset
+    
+    def fit(self, X: pd.DataFrame, n_epochs, d_learning_rate=0.0002,
+            g_learning_rate=0.00002, save_checkpoints=False, enable_prints=False, *args, **kwargs) -> None:
+        #prepocesing dat
+        dataset = self._preprocess_data(X)
+        
+        self._discriminator = self._gan_ad.get_layer('discriminator')
+        self._generator = self._gan_ad.get_layer('generator')
+        
+        self._discriminator_optimizer = RMSprop(learning_rate=d_learning_rate)
+        self._generator_optimizer = Adam(learning_rate=g_learning_rate, beta_1=0.5) 
+
+        self._train(dataset, n_epochs, enable_prints)
+        pass
+    
+    def save_model(self, file_name):
+        self._gan_ad.save(file_name)
+    
+    # implementation from https://github.com/ananda1996ai/MMD-AutoEncoding-GAN-MAEGAN-/blob/master/maegan_final.py
+    # Maximum Mean Discrepancy Auto-Encoding Generative Adversarial Networks (MAEGAN)
+    def _rbf_kernel(self, x, y):
+        x_size = tf.shape(x)[0]
+        y_size = tf.shape(y)[0]
+        dim = tf.shape(x)[1]
+
+        tiled_x = tf.tile(tf.reshape(x, tf.stack([x_size, 1, dim])), tf.stack([1, y_size, 1]))
+        tiled_y = tf.tile(tf.reshape(y, tf.stack([1, y_size, dim])), tf.stack([x_size, 1, 1]))
+
+        return tf.exp(-tf.reduce_mean(tf.square(tiled_x - tiled_y), axis=2) / tf.cast(dim, tf.float64))
+
+    def _get_mmd_similarity(self, x, y):
+        xy_kernel = self._rbf_kernel(x, y)
+        return xy_kernel
+    
+    def _find_mapping(self, sample):
+        tolerance = 0.05
+
+        z_init = tf.random.normal([self._window_size, self._latent_dim])
+        z_opt = tf.Variable(z_init, trainable=True, dtype=tf.float32)
+
+        mse = tf.keras.losses.MeanSquaredError()
+
+        def get_loss():
+            generated_sample = self._generator(tf.reshape(z_opt, [1, self._window_size, self._latent_dim]), training = False)[0]
+            similarity_per_sample = self._get_mmd_similarity(tf.cast(sample, tf.float64), tf.cast(generated_sample, tf.float64))
+            reconstruction_loss_per_sample = 1 - similarity_per_sample
+            return reconstruction_loss_per_sample
+
+        for i in range(2000):
+            RMSprop(learning_rate=0.01).minimize(get_loss, var_list=[z_opt])
+            if(i%100 == 0):
+                tf.print('loss celkovo: ', tf.reduce_mean(get_loss()))
+            if(tf.reduce_mean(get_loss()) < tolerance):
+                break
+
+        tf.print('final loss: ', tf.reduce_mean(get_loss()))
+        tf.print(z_opt)
+        return z_opt
+    
     def predict_anomaly_scores(
-        self, X: pd.DataFrame, *args, **kwargs
-    ) -> pd.Series:
-        # TODO: return predicted anomaly scores for the given samples
-        pass
+            self, X: pd.DataFrame, *args, **kwargs
+        ) -> pd.Series:
+        
+        X = self._scaler.transform(X)
+        z_mapping = self._find_mapping(X)
+        lamda = 0.5
 
-    def fit(self, X: pd.DataFrame, *args, **kwargs) -> None:
-        # TODO: perform training
-        pass
+        # calculate the residuals
+        generated_sample = self._generator(tf.reshape(z_mapping, [1, self._window_size, self._latent_dim]), training = False)[0]
+        print('sample')
+        print(X)
+        window = pd.DataFrame(X.numpy())
+        window.plot(figsize=(10,5))
+        plt.show()
+        print('generated_sample')
+        print(generated_sample)
+        window = pd.DataFrame(generated_sample.numpy())
+        window.plot(figsize=(10,5))
+        plt.show()
+
+        residuals = tf.math.reduce_sum(abs(tf.cast(X, tf.float64) - tf.cast(generated_sample, tf.float64)), axis=1)
+        print('residuals: ')
+        print(residuals)
+
+        # calculate the discrimination results
+        real_prob = self._discriminator(tf.reshape(generated_sample, [1, generated_sample.shape[0], generated_sample.shape[1]]), training = False)[0]
+        print('real_prob')
+        print(real_prob)
+
+        # obtain anomaly score
+        # https://ieeexplore.ieee.org/stamp/stamp.jsp?tp=&arnumber=8942842&tag=1 1 - real_prob makes more sense
+        anomaly_score = tf.add((1-lamda)*residuals, tf.cast(lamda*(1-real_prob), tf.float64))
+        return anomaly_score
+    
+    def identify_anomaly(self, X, treshold = 0.9):
+        anomaly_score = self.get_anomaly_score(X)
+        cross_entropy = np.log(anomaly_score)
+        print('cross_entropy')
+        print(cross_entropy)
+        identified = (cross_entropy > treshold).astype(int)
+        return identified
