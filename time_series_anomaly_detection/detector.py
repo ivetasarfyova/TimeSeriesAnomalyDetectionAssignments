37b04913a01136792bde96f518e3aaddc024adf8
Bc. Iveta Šárfyová <sarfyive@fit.cvut.cz>
2021-05-01 13:19:17 +0200

    anomaly score for data of various length

--- time_series_anomaly_detection/detector.py
+++ time_series_anomaly_detection/detector.py
@@ -1,4 +1,4 @@
-from typing import Optional, Iterable
+from typing import Optional, Iterable, Tuple
 import pandas as pd
 import numpy as np
 import tensorflow as tf
@@ -15,7 +15,6 @@ import random
 from time_series_anomaly_detection.abstractions import (
     TimeSeriesAnomalyDetector
 )
-
 
 class GAN_AD(TimeSeriesAnomalyDetector):
     """
@@ -55,8 +54,10 @@ class GAN_AD(TimeSeriesAnomalyDetector):
         self._bce = BinaryCrossentropy()
         
         self._gan_ad = self._build_gan_ad()
+        self._discriminator = self._gan_ad.get_layer('discriminator')
+        self._generator = self._gan_ad.get_layer('generator')
     
-    def _build_discriminator(self):
+    def _build_discriminator(self) -> keras.Model:
         data = Input(shape=(self._window_size, self._n_features))
 
         x = LSTM(150)(data)
@@ -70,7 +71,7 @@ class GAN_AD(TimeSeriesAnomalyDetector):
         model = keras.Model(inputs=[data], outputs=[decision], name='discriminator')
         return model
     
-    def _build_generator(self):
+    def _build_generator(self) -> keras.Model:
         z = Input(shape=(self._window_size, self._latent_dim))
 
         x = Conv1D(30, padding='same', kernel_size=3) (z)
@@ -84,7 +85,7 @@ class GAN_AD(TimeSeriesAnomalyDetector):
         model = keras.Model(inputs=[z], outputs=[series], name='generator')
         return model
     
-    def _build_gan_ad(self):
+    def _build_gan_ad(self) -> keras.Model:
         generator = self._build_generator()
         discriminator = self._build_discriminator()
 
@@ -96,17 +97,17 @@ class GAN_AD(TimeSeriesAnomalyDetector):
         model = keras.Model(inputs=[z], outputs=[generated_series, generated_series_eval], name='gan')
         return model
     
-    def _get_discriminator_loss(self, real, generated):
+    def _get_discriminator_loss(self, real: tf.Tensor, generated: tf.Tensor) -> tf.Tensor:
         real_loss = self._bce(tf.ones_like(real), real)
         generated_loss = self._bce(tf.zeros_like(generated), generated)
         d_loss = real_loss + generated_loss
         return d_loss
     
-    def _get_generator_loss(self, generated):
+    def _get_generator_loss(self, generated: tf.Tensor) -> tf.Tensor:
         return self._bce(tf.ones_like(generated), generated)
     
     @tf.function
-    def _fit_batch(self, batch, z):
+    def _fit_batch(self, batch: tf.Tensor, z: tf.Tensor) -> Tuple:
         with tf.GradientTape(persistent=True) as gt:
 
             generated_series = self._generator(z, training = True)
@@ -125,10 +126,13 @@ class GAN_AD(TimeSeriesAnomalyDetector):
 
         return (d_loss, g_loss)
     
-    def save_weights(self, file_name):
+    def save_weights(self, file_name:str) -> None:
         self._gan_ad.save_weights(file_name)
+       
+    def load_weights(self, file_name:str) -> None:
+        self._gan_ad.load_weights(file_name)
     
-    def _train(self, dataset, epochs, enable_prints=False):
+    def _train(self, dataset, epochs: int, enable_prints: Optional[bool] = False):
         loss_history = []
         for epoch in range(epochs):
             batch_loss_history = []
@@ -137,9 +141,9 @@ class GAN_AD(TimeSeriesAnomalyDetector):
                 d_loss, g_loss = self._fit_batch(batch, z)
                 batch_loss_history.append([d_loss.numpy(), g_loss.numpy()])
             loss_history.append(np.mean(batch_loss_history, axis = 0))
-            if ((epoch != 0) & (epoch%500 == 0) & enable_prints):
+            if ((epoch != 0) and (epoch%500 == 0) and enable_prints):
                 print(epoch, ". epoch")
-                print("Batch loss (disc_loss, gen_loss) :", np.mean(batch_loss_history, axis=0))
+                print("Batch loss [disc_loss, gen_loss] :", np.mean(batch_loss_history, axis=0))
                 plt.plot(np.array([loss[0] for loss in loss_history]), color='green')
                 plt.plot(np.array([loss[1] for loss in loss_history]), color='blue')
                 plt.show()
@@ -164,10 +168,13 @@ class GAN_AD(TimeSeriesAnomalyDetector):
                     axs[int(i%3), int(i/3)].plot(generated_sample.numpy())
                 plt.show()
                 print()
-            if (epoch%1000 == 0):
+            if ((epoch != 0) and (epoch%1000 == 0)):
                 self.save_weights("gan_ad_" + str(date.today()) + "_epoch_" + str(epoch) + ".h5")
-
-    def _preprocess_data(self, data: pd.DataFrame):
+                
+    def fit_scaler(self, data: pd.DataFrame) -> None:
+        self._scaler.fit(data)        
+                
+    def _preprocess_data(self, data: pd.DataFrame) -> tf.data.Dataset:
         # TODO remove idcols
         data_scaled = self._scaler.fit_transform(data)
         # TODO for different time series types
@@ -178,13 +185,11 @@ class GAN_AD(TimeSeriesAnomalyDetector):
                                                                batch_size=self._batch_size, shuffle=True, seed=6)
         return dataset
     
-    def fit(self, X: pd.DataFrame, n_epochs, d_learning_rate=0.0002,
-            g_learning_rate=0.00002, save_checkpoints=False, enable_prints=False, *args, **kwargs) -> None:
-        #prepocesing dat
-        dataset = self._preprocess_data(X)
+    def fit(self, X: pd.DataFrame, n_epochs: int, d_learning_rate: float = 0.0002,
+            g_learning_rate: float = 0.00002, save_checkpoints: Optional[bool] = False,
+            enable_prints: Optional[bool] = False, *args, **kwargs) -> None:
         
-        self._discriminator = self._gan_ad.get_layer('discriminator')
-        self._generator = self._gan_ad.get_layer('generator')
+        dataset = self._preprocess_data(X)
         
         self._discriminator_optimizer = RMSprop(learning_rate=d_learning_rate)
         self._generator_optimizer = Adam(learning_rate=g_learning_rate, beta_1=0.5) 
@@ -192,12 +197,12 @@ class GAN_AD(TimeSeriesAnomalyDetector):
         self._train(dataset, n_epochs, enable_prints)
         pass
     
-    def save_model(self, file_name):
+    def save_model(self, file_name: str) -> None:
         self._gan_ad.save(file_name)
     
     # implementation from https://github.com/ananda1996ai/MMD-AutoEncoding-GAN-MAEGAN-/blob/master/maegan_final.py
     # Maximum Mean Discrepancy Auto-Encoding Generative Adversarial Networks (MAEGAN)
-    def _rbf_kernel(self, x, y):
+    def _rbf_kernel(self, x: tf.Tensor, y: tf.Tensor) -> tf.Tensor:
         x_size = tf.shape(x)[0]
         y_size = tf.shape(y)[0]
         dim = tf.shape(x)[1]
@@ -207,11 +212,11 @@ class GAN_AD(TimeSeriesAnomalyDetector):
 
         return tf.exp(-tf.reduce_mean(tf.square(tiled_x - tiled_y), axis=2) / tf.cast(dim, tf.float64))
 
-    def _get_mmd_similarity(self, x, y):
+    def _get_mmd_similarity(self, x: tf.Tensor, y: tf.Tensor) -> tf.Tensor:
         xy_kernel = self._rbf_kernel(x, y)
         return xy_kernel
     
-    def _find_mapping(self, sample):
+    def _find_mapping(self, sample: tf.Tensor) -> tf.Tensor:
         tolerance = 0.05
 
         z_init = tf.random.normal([self._window_size, self._latent_dim])
@@ -225,9 +230,9 @@ class GAN_AD(TimeSeriesAnomalyDetector):
             reconstruction_loss_per_sample = 1 - similarity_per_sample
             return reconstruction_loss_per_sample
 
-        for i in range(2000):
+        for i in range(1000):
             RMSprop(learning_rate=0.01).minimize(get_loss, var_list=[z_opt])
-            if(i%100 == 0):
+            if(i%200 == 0):
                 tf.print('loss celkovo: ', tf.reduce_mean(get_loss()))
             if(tf.reduce_mean(get_loss()) < tolerance):
                 break
@@ -236,23 +241,22 @@ class GAN_AD(TimeSeriesAnomalyDetector):
         tf.print(z_opt)
         return z_opt
     
-    def predict_anomaly_scores(
+    def predict_window_anomaly_scores(
             self, X: pd.DataFrame, *args, **kwargs
-        ) -> pd.Series:
+        ) -> tf.Tensor:
         
-        X = self._scaler.transform(X)
         z_mapping = self._find_mapping(X)
         lamda = 0.5
 
         # calculate the residuals
         generated_sample = self._generator(tf.reshape(z_mapping, [1, self._window_size, self._latent_dim]), training = False)[0]
+        
         print('sample')
-        print(X)
-        window = pd.DataFrame(X.numpy())
+        window = pd.DataFrame(X)
         window.plot(figsize=(10,5))
         plt.show()
+        
         print('generated_sample')
-        print(generated_sample)
         window = pd.DataFrame(generated_sample.numpy())
         window.plot(figsize=(10,5))
         plt.show()
@@ -262,7 +266,8 @@ class GAN_AD(TimeSeriesAnomalyDetector):
         print(residuals)
 
         # calculate the discrimination results
-        real_prob = self._discriminator(tf.reshape(generated_sample, [1, generated_sample.shape[0], generated_sample.shape[1]]), training = False)[0]
+        real_prob = self._discriminator(tf.reshape(generated_sample, [1, generated_sample.shape[0], generated_sample.shape[1]]),
+                                        training = False)[0]
         print('real_prob')
         print(real_prob)
 
@@ -270,9 +275,29 @@ class GAN_AD(TimeSeriesAnomalyDetector):
         # https://ieeexplore.ieee.org/stamp/stamp.jsp?tp=&arnumber=8942842&tag=1 1 - real_prob makes more sense
         anomaly_score = tf.add((1-lamda)*residuals, tf.cast(lamda*(1-real_prob), tf.float64))
         return anomaly_score
+        
+    def predict_anomaly_scores(
+            self, X: pd.DataFrame, *args, **kwargs
+        ) -> pd.Series:
+        
+        if(X.shape[0] < self._window_size):
+            print("Not enough data to predict anomaly scores.")
+            return
+        
+        X = self._scaler.transform(X)
+        
+        resid = X.shape[0]%self._window_size
+        if(resid != 0):
+            print("Ignoring last " + str(resid) + " samples (incomplete window).")
+        
+        # TODO split to windows and for each window
+        windows = [pd.DataFrame(X[i:i+self._window_size]) for i in range(0,X.shape[0]-resid,self._window_size)]
+        
+        anomaly_score = tf.concat([self.predict_window_anomaly_scores(X_window) for X_window in windows], -1)
+        return pd.Series(anomaly_score)
     
-    def identify_anomaly(self, X, treshold = 0.9):
-        anomaly_score = self.get_anomaly_score(X)
+    def identify_anomaly(self, X: pd.DataFrame, treshold: Optional[int] = 1) -> np.array:
+        anomaly_score = self.predict_anomaly_scores(X)
         cross_entropy = np.log(anomaly_score)
         print('cross_entropy')
         print(cross_entropy)
